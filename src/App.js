e946f40d9f11d391750132a59b94613d8a6b3536
kimhyeji <gpwl5589@gamil.com>
2022-12-15 20:07:20 +0900

    팝업과 닫기버튼

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import StopWatch from "./StopWatch";
 import NumberCounter from "./NumberCounter";
+import Popup from "./Popup";
 
 function App() {
   return (
     <>
       {/* <StopWatch /> */}
       {/* <NumberCounter /> */}
-      <h1 className="text-red-600 text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <button className="btn btn-primary">Button</button>
+      <Popup />
     </>
   );
 }
