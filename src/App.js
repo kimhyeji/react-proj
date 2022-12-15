468f2132a2d5ab13c86bdc79bf97f11ed7586627
kimhyeji <gpwl5589@gamil.com>
2022-12-15 21:46:47 +0900

    상품리스트 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import StopWatch from "./StopWatch";
 import NumberCounter from "./NumberCounter";
 import Popup from "./Popup";
+import ProductListItem from "./ProductListItem";
 
 function App() {
   return (
     <>
       {/* <StopWatch /> */}
       {/* <NumberCounter /> */}
-      <Popup />
+      {/* <Popup /> */}
+      <div style={{ display: "flex", gap: "10px" }}>
+        <ProductListItem
+          imgNo={201}
+          name="MAC BOOK AIR"
+          productPriceFormatted={"1,140,000"}
+        />
+
+        <ProductListItem
+          imgNo={1}
+          name="MAC BOOK PRO"
+          productPriceFormatted={"3,320,000"}
+        />
+
+        <ProductListItem
+          imgNo={2}
+          name="MAC BOOK PLUS"
+          productPriceFormatted={"4,340,000"}
+        />
+      </div>
     </>
   );
 }
