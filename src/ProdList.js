31674d5b2486d26f080e71efc3995d39680057f1
kimhyeji <gpwl5589@gamil.com>
2022-12-16 20:22:50 +0900

    반응형 상품리스트

---
+++ src/ProdList.js
@@ -0,0 +1,56 @@
+import ProdListItem from "./ProdListItem";
+
+function ProdList({ className }) {
+  return (
+    <>
+      <div className={className}>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-[20px]">
+          <li>
+            <ProdListItem
+              imgNo={201}
+              name="PRODUCT 1"
+              productPriceFormatted={"140,000"}
+            />
+          </li>
+          <li>
+            <ProdListItem
+              imgNo={1}
+              name="PRODUCT 2"
+              productPriceFormatted={"320,000"}
+            />
+          </li>
+          <li>
+            <ProdListItem
+              imgNo={2}
+              name="PRODUCT 3"
+              productPriceFormatted={"340,000"}
+            />
+          </li>
+          <li>
+            <ProdListItem
+              imgNo={201}
+              name="PRODUCT 1"
+              productPriceFormatted={"140,000"}
+            />
+          </li>
+          <li>
+            <ProdListItem
+              imgNo={1}
+              name="PRODUCT 2"
+              productPriceFormatted={"320,000"}
+            />
+          </li>
+          <li>
+            <ProdListItem
+              imgNo={2}
+              name="PRODUCT 3"
+              productPriceFormatted={"340,000"}
+            />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default ProdList;
