65ab945e0526dcc4cffce3ff59b690cb5daceb95
kimhyeji <gpwl5589@gamil.com>
2022-12-29 21:02:58 +0900

    useEffect를 사용하여 한번 알림된 내용이 다시 알림되지 않도록

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
-import StopWatch from "./StopWatch";
-import NumberCounter from "./NumberCounter";
-import Popup from "./Popup";
-import ProdList from "./ProdList";
-import NoRecord from "./NoRecord";
-import FormEx from "./FormEx";
-import RefRemoveEx from "./RefRemoveEx";
-import RefModifyEx from "./RefModifyEx";
-import Notice from "./Notice";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 
-function App() {
+let AppCallCount = 0;
+let SubCallCount = 0;
+
+function Sub({ appNo }) {
+  SubCallCount++;
+  console.log(`Sub가 ${SubCallCount}번 실행됨!`);
+
+  const [no, setNo] = useState(0);
+  const [no2, setNo2] = useState(0);
+
+  useEffect(() => {
+    console.log("effect 1 : 단 한 번 실행");
+  }, []);
+
+  useEffect(() => {
+    console.log("effect 2 : 부모(App)의 appNo가 바뀔 때 마다 실행");
+  }, [appNo]);
+
+  useEffect(() => {
+    console.log("effect 3 : 나(Sub)의 no가 바뀔 때 마다 실행");
+  }, [no]);
+
+  useEffect(() => {
+    console.log("effect 4 : appNo 혹은 no가 변경될 때 마다 실행");
+  }, [appNo, no]);
+
+  useEffect(() => {
+    console.log("effect 5 : 매 번 실행");
+  });
+
   return (
     <>
-      <Notice />
+      <div style={{ border: "10px solid blue", padding: 10 }}>
+        App no : {appNo}
+        <hr />
+        <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
+        <button onClick={() => setNo2(no2 + 1)}>버튼2 : {no2}</button>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  AppCallCount++;
+  console.log(`App이 ${AppCallCount}번 실행됨!`);
+
+  const [no, setNo] = useState(0);
+
+  return (
+    <>
+      <div style={{ border: "10px solid red", padding: 10 }}>
+        <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
+        <hr />
+        <Sub appNo={no} />
+      </div>
     </>
   );
 }
